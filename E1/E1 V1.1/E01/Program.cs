6fb5228958c7e9051bcfceb695a5e94b8181a27f
Max <maxi.ng@hotmail.com>
2018-12-12 17:53:49 -0300

    first upload

---
+++ E1/E1 V1.1/E01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "EJERCICIO 01";
+            //////////////////////////////
+
+
+
+            string[] numeros = new string[5];
+
+            numeros = ArrayNumeros.SolicitarNumeros(5, 3, "ha ingresado un valor no valido, intente nuevamente: ");
+
+            if (numeros != null)
+                ArrayNumeros.MostrarNumeros(numeros);
+
+        }
+    }
+}
+
+
+
+
