6599eb67e1244363ff1092b05be8b1544c1f9931
Max <maxi.ng@hotmail.com>
2018-12-12 19:41:33 -0300

    -

---
+++ E6/E6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int año;
+
+            while (Program.ObtenerNumeroDelUsuario("ingrese un año:", 3, "ha ingresado un valor incorrecto. intente nuevamente: ", out año))
+                Console.WriteLine(CalcularAñoBisiesto(año));
+        }
+
+        public static string CalcularAñoBisiesto(int año)
+        {
+            int[] modulos = new int[3];
+            modulos[0] = año % 4;
+            modulos[1] = año % 100;
+            modulos[2] = año % 400;
+
+            if ((modulos[1] == 0 && modulos[2] == 0) || (modulos[0] == 0 && modulos[1] != 0))
+                return ("Es Bisiesto");
+            else
+                return ("No es Bisiesto");
+        }
+        public static bool ObtenerNumeroDelUsuario(string request, int intentos, string msgError, out int numero)
+        {
+            Console.WriteLine(request);
+            while (!int.TryParse(Console.ReadLine(), out numero) || numero < 1)
+            {
+                if (intentos == 0)
+                    return false;
+
+                Console.WriteLine(msgError + "({0})", intentos);
+                intentos--;
+            }
+            return true;
+        }
+    }
+}
