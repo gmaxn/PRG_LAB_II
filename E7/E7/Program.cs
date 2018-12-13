feba94e4007ce4fee2a01ca8626efcf6d14bf437
Max <maxi.ng@hotmail.com>
2018-12-12 23:42:44 -0300

    -

---
+++ E7/E7/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fecha;
+            if(PedirFechaDeNacimiento(1, out fecha))
+            {
+                Console.WriteLine("la persona vivio {0} días", CalcularEdad(fecha));
+            }
+        }
+
+        public static int CalcularEdad(DateTime fechaDeNacimiento)
+        {
+            TimeSpan time = DateTime.Now - fechaDeNacimiento;
+            return time.Days;
+        }
+        public static bool PedirFechaDeNacimiento(int intentos, out DateTime fechaDeNacimiento)
+        {
+            int[] diaMesAño = new int[3];
+            Console.WriteLine("presione una tecla para ingresar fecha");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("presione una tecla para ingresar fecha");
+            if (Program.ObtenerNumeroDelUsuario("dia: ", intentos, "dia incorrecto", out diaMesAño[0]))
+            {
+                if (Program.ObtenerNumeroDelUsuario("mes: ", intentos, "mes incorrecto", out diaMesAño[1]))
+                {
+                    if (Program.ObtenerNumeroDelUsuario("año: ", intentos, "año incorrecto", out diaMesAño[2]))
+                    {
+                        fechaDeNacimiento = new DateTime(diaMesAño[2], diaMesAño[1], diaMesAño[0]);
+                        return true;
+                    }
+                }
+            }
+            fechaDeNacimiento = new DateTime(0, 0, 0);
+            return false;
+        }
+        public static bool ObtenerNumeroDelUsuario(string request, int intentos, string msgError, out int numero)
+        {
+            Console.Write(request);
+            while (!int.TryParse(Console.ReadLine(), out numero) || numero < 1)
+            {
+                if (intentos == 0)
+                    return false;
+
+                Console.WriteLine(msgError + "({0})", intentos);
+                intentos--;
+            }
+            return true;
+        }
+    }
+
+
+
+}
---
+++ E8/E8/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado e = new Empleado();
+        }
+
+
+        public static bool ObtenerDecimal(string request, int intentos, string msgError, out float numero)
+        {
+            Console.WriteLine(request);
+            while (!float.TryParse(Console.ReadLine(), out numero) || numero <= 0)
+            {
+                if (intentos == 0)
+                    return false;
+
+
+                Console.WriteLine(msgError + "({0})", intentos);
+                intentos--;
+            }
+            return true;
+        }
+        public static bool ObtenerNumeroNatural(string request, int intentos, string msgError, out int numero)
+        {
+            Console.WriteLine(request);
+            while (!int.TryParse(Console.ReadLine(), out numero) || numero < 1)
+            {
+                if (intentos == 0)
+                    return false;
+
+
+                Console.WriteLine(msgError + "({0})", intentos);
+                intentos--;
+            }
+            return true;
+        }
+    }
+    public class Empleado
+    {
+        #region ** CLASS CORE **
+        // >> Campos
+        private string nombre;
+        private int antiguedad;
+        private int cantHorasTrabajadas;
+
+        // >> Propiedades
+        public string Nombre
+        {
+            get { return this.nombre; }
+        }
+        public int Antiguedad
+        {
+            get { return this.antiguedad; }
+        }
+        public int CantHorasTrabajadas
+        {
+            get { return this.cantHorasTrabajadas; }
+        }
+
+
+        // >> Constructores
+        public Empleado()
+        { }
+        public Empleado(string nombre, int antiguedad, int cantHorasTrabajadas)
+        {
+            this.nombre = nombre;
+            this.antiguedad = antiguedad;
+            this.cantHorasTrabajadas = cantHorasTrabajadas;
+        }
+
+        // >> Mostrar Datos
+        public string ShowData()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            return sb.ToString();
+        }
+        #endregion
+
+        // >> Metodos
+        public void ObtenerDatos()
+        {
+
+            //if(Program.)
+        }
+
+        public string ObtenerNombre()
+        {
+            Console.Write("nombre: ");
+            string nombre = Console.ReadLine();
+
+            if (nombre.Length < 10 && nombre != null)
+            {
+                foreach (char c in nombre)
+                {
+                    if ((c > 'A' || c < 'Z') || (c > 'a' || c < 'z'))
+                    {
+                        nombre = null;
+                    }
+                }
+
+            }
+            else
+            {
+                nombre = null;
+            }
+
+            return nombre;
+            
+            
+            if(Console.ReadLine().ToStri < 10)
+
+
+        }
+
+
+    }
+}
