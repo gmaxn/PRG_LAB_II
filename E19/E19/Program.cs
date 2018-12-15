18ebec9431f271b9d6fa96c56be609ebf8fb1aed
Max <maxi.ng@hotmail.com>
2018-12-14 22:06:21 -0300

    finalizado Ok

---
+++ E19/E19/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador s1 = new Sumador();
+            Sumador s2 = new Sumador(250);
+
+            for (int i = 0; i < 250; i++)
+                s1.Sumar(1, 1);
+
+            if(s1 | s2)
+            {
+                Console.WriteLine("Ambos sumadores tienen igual cantidad de sumas");
+            }
+
+            Console.WriteLine(s2.Sumar("Hello", "World!"));
+        }
+    }
+}
---
+++ E19/E19/Sumador.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E19
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+
+        public Sumador()
+            : base()
+        {
+            this.cantidadSumas = 0;
+        }
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+        public static explicit operator int(Sumador s)
+        {
+            return s.cantidadSumas;
+        }
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            if (s1.cantidadSumas == s2.cantidadSumas)
+                return true;
+            return false;
+        }
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            return (s1.cantidadSumas + s2.cantidadSumas);
+        }
+        public long Sumar(long a, long b)
+        {
+            cantidadSumas++;
+            return a + b;
+        }
+        public string Sumar(string a, string b)
+        {
+            cantidadSumas++;
+            return a + b;
+        }
+    }
+}
