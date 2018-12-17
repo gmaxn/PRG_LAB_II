667447632d247032dd0c70ed300bbfbf7f8ad8d9
Max <maxi.ng@hotmail.com>
2018-12-17 16:25:35 -0300

    finalizado, perdida de precision float - double

--- E20/E20/Program.cs
+++ E20/E20/Program.cs
@@ -27,19 +27,19 @@ namespace E20
             Console.WriteLine("ARS{0} + ARS{1}: ARS{2}", (double)p2, (double)p2, (double)(p2 + p2));
             Console.WriteLine("ARS{0} + USD{1}: ARS{2}", (double)p2, (double)d2, (double)(p2 + d2));
             Console.WriteLine("ARS{0} + EUR{1}: ARS{2}", (double)p2, (double)e2, (double)(p2 + e2));
-            Console.WriteLine("*****************************************");
+            Console.WriteLine("******************************************");
 
             Console.WriteLine("OPERACIONES MIXTAS EURO");
             Console.WriteLine("EUR{0} + EUR{1}: EUR{2}", (double)e2, (double)e2, (double)(e2 + e2));
             Console.WriteLine("EUR{0} + USD{1}: EUR{2}", (double)e2, (double)d2, (double)(e2 + d2));
             Console.WriteLine("EUR{0} + ARS{1}: EUR{2}", (double)e1, (double)p2, (double)(e2 + p2));
-            Console.WriteLine("*****************************************");
+            Console.WriteLine("******************************************");
 
             Console.WriteLine("OPERACIONES MIXTAS DOLAR");
             Console.WriteLine("USD{0} + USD{1}: USD{2}", (double)d2, (double)d2, (double)(d2 + d2));
             Console.WriteLine("USD{0} + ARS{1}: USD{2}", (double)d2, (double)p2, (double)(d2 + p2));
             Console.WriteLine("USD{0} + EUR{1}: USD{2}", (double)d2, (double)e2, (double)(d2 + e2));
-            Console.WriteLine("*****************************************");
+            Console.WriteLine("******************************************");
 
             Console.ReadKey();
 
