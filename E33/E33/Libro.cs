934f4f802f8f885006ba1ff55f05ae3041ce826d
Max <maxi.ng@hotmail.com>
2018-12-25 19:11:14 -0300

    finalizado Ok

---
+++ E33/E33/Libro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E33
+{
+    public class Libro
+    {
+        private List<string> paginas;
+
+        public Libro()
+        {
+            this.paginas = new List<string>(100);
+        }
+        
+        public string this[int i]
+        {
+            get
+            {
+                if (i >= 0 && i < this.paginas.Count)
+                {
+                    return paginas[i];
+                }
+                return "";
+            }
+            set
+            {
+                if (i >= 0)
+                {
+                    if (i < this.paginas.Count)
+                    {
+                        paginas[i] = value;
+                    }
+                    paginas.Add(value);
+                }
+            }
+        }
+    }
+}
---
+++ E33/E33/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro l = new Libro();
+
+            Console.WriteLine(l[0]);
+            Console.WriteLine(l[1]);
+            l[0] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam facilisis, urna nec sodales faucibus, nisl enim convallis mi, in vulputate nibh justo at libero.";
+            l[1] = "Nulla rutrum nisl iaculis commodo luctus. Vestibulum facilisis, urna sit amet placerat luctus, augue risus accumsan nulla, a accumsan turpis felis nec purus. Vestibulum et facilisis augue. Donec faucibus sagittis pharetra.";
+            Console.WriteLine(l[0]);
+            Console.WriteLine(l[1]);
+        }
+    }
+}
