3a3c01bdd5a6540b1b6b95091560e2f32a4b2e19
Max <maxi.ng@hotmail.com>
2018-12-26 08:51:37 -0300

    -

---
+++ E41/CentralTelefonicaFormulario/FrmMenu.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using E40;
+
+namespace CentralTelefonicaFormulario
+{
+    public partial class FrmMenu : Form
+    {
+        public Centralita centralita;
+        public FrmMenu()
+        {
+            InitializeComponent();
+            this.centralita = new Centralita();
+        }
+        public FrmMenu(Centralita c)
+        {
+            InitializeComponent();
+            this.centralita = c;
+        }
+
+        private void btn_GenerarLLamada_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            FrmLlamador llamador = new FrmLlamador(this.centralita);
+            llamador.ShowDialog();
+        }
+        private void btn_FacturacionTotal_Click(object sender, EventArgs e)
+        {
+            this.Hide();       
+            FrmMostrar mostrar = new FrmMostrar(this.centralita.ToString(), this.centralita);
+            mostrar.ShowDialog();
+        }
+
+        private void btn_FacturacionLocal_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("FACTURACION POR LLAMADAS LOCALES");
+            sb.AppendLine("********************************");
+
+            foreach (Llamada l in this.centralita.Llmadas)
+            {
+                if(l is Local)
+                {
+                    sb.AppendLine(l.ToString());
+                    sb.AppendLine("--------------------------------");
+                }
+            }
+
+            FrmMostrar mostrar = new FrmMostrar(sb.ToString(), this.centralita);
+            this.Hide();
+            mostrar.ShowDialog();
+        }
+
+        private void btn_FacturacionProvincial_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("FACTURACION POR LLAMADAS PROVINCIALES");
+            sb.AppendLine("*************************************");
+
+            foreach (Llamada l in this.centralita.Llmadas)
+            {
+                if (l is Provincial)
+                {
+                    sb.AppendLine(l.ToString());
+                    sb.AppendLine("--------------------------------");
+                }
+            }
+
+            FrmMostrar mostrar = new FrmMostrar(sb.ToString(), centralita);
+            this.Hide();
+            mostrar.ShowDialog();
+        }
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ E41/E41/Provincial.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E41
+{
+    public class Provincial : Llamada
+    {
+        public enum Franja { Franja_1, Franja_2, Franja_3 }
+        protected Franja _franjaHoraria;
+
+        public override float CostoLlamada
+        {
+            get { return this.CalcularCosto(); }
+        }
+
+        public Provincial(string origen, string destino, float duracion, Franja miFranja)
+            : base(origen, destino, duracion)
+        {
+            this._franjaHoraria = miFranja;
+        }
+        public Provincial(Llamada llamada, Franja miFranja)
+            : this(llamada.NroOrigen, llamada.NroDestino, llamada.Duracion, miFranja)
+        { }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+        protected override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(base.Mostrar());
+            sb.AppendLine("Franja:      " + this._franjaHoraria);
+            sb.AppendLine("Costo:       $" + this.CalcularCosto());
+            sb.AppendLine();
+            return sb.ToString();
+        }
+
+        private float CalcularCosto()
+        {
+            float aux;
+
+            switch (this._franjaHoraria)
+            {
+                case Franja.Franja_1:
+                    aux = (base._duracion * 0.99F);
+                    break;
+
+                case Franja.Franja_2:
+                    aux = (base._duracion * 1.25F);
+                    break;
+
+                case Franja.Franja_3:
+                    aux = (base._duracion * 0.66F);
+                    break;
+
+                default:
+                    throw new NotImplementedException("franja inexistente");
+            }
+            return aux;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null && obj is Provincial)
+            {
+                if ((this._nroOrigen.CompareTo(((Provincial)obj).NroOrigen) == 0)
+                && (this._nroDestino.CompareTo(((Provincial)obj).NroDestino) == 0))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
