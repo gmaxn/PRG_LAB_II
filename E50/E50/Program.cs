a4d82dd8b908d446a9cc21ef10d3f09721d1e937
Max <maxi.ng@hotmail.com>
2018-12-26 18:01:51 -0300

    finalizado Ok

---
+++ E50/E50/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "string";
+            int integer = 1;
+
+            Serializar<int, string> S1 = new Serializar<int, string>();
+            GuardarTexto<string, string> G1 = new GuardarTexto<string, string>();
+
+            Console.WriteLine(S1.Leer());
+            Console.WriteLine(S1.Guardar(integer));
+
+            Console.WriteLine(G1.Leer());
+            Console.WriteLine(G1.Guardar(str));
+        }
+    }
+}
