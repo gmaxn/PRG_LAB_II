605024da1e6dc0a35e6711b4faa92bb625a65417
Max <maxi.ng@hotmail.com>
2018-12-27 20:57:18 -0300

    finalizado Ok

---
+++ E53/E53/Cartuchera1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E53
+{
+    public class Cartuchera1
+    {
+        bool flag = true;
+        List<IAcciones> c;
+
+        public bool ProbarElementos()
+        {
+            foreach (IAcciones aux in this.c)
+            {
+                if (aux.UnidadesDeEscritura > 1)
+                    aux.UnidadesDeEscritura--;
+                else
+                {
+                    aux.Recargar(10);
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+    }
+}
---
+++ E53/E53/Cartuchera2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E53
+{
+    public class Cartuchera2
+    {
+        private List<Boligrafo> _boligrafos;
+        private List<Lapiz> _lapices;
+
+        public Cartuchera2()
+        {
+            this._boligrafos = new List<Boligrafo>();
+            this._lapices = new List<Lapiz>();
+        }
+
+        public bool ProbarElementos()
+        {
+            int boligrafosLength = _boligrafos.Count;
+            int lapicesLength = _lapices.Count;
+
+            foreach (Boligrafo b in this._boligrafos)
+            {
+                if (b.UnidadesDeEscritura > 0.1F)
+                {
+                    b.UnidadesDeEscritura -= 0.1F;
+                    boligrafosLength--;
+                }
+            }
+            foreach (Lapiz l in this._lapices)
+            {
+                if (((IAcciones)l).UnidadesDeEscritura > 0.3F)
+                {
+                    ((IAcciones)l).UnidadesDeEscritura -= 0.3F;
+                    lapicesLength--;
+                }
+            }
+            if (boligrafosLength == 0 && lapicesLength == 0)
+                return true;
+
+            return false;
+        }
+    }
+}
