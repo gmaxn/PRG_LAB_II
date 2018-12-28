fc7abf41ddefe50d538c2473ff8d8fcef3744929
Max <maxi.ng@hotmail.com>
2018-12-28 12:30:21 -0300

    finalizado Ok

---
+++ E55/E55/FallaLogException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E55
+{
+    public class FallaLogException : Exception
+    {
+        public FallaLogException(string msg, Exception inner)
+            : base(msg, inner)
+        { }
+
+        public FallaLogException(string msg)
+            : base(msg)
+        { }
+
+
+        public override string ToString()
+        {
+            return "Mensaje: " + base.Message + ", Origen del error: " + base.Source;
+        }
+    }
+}
---
+++ E55/E55/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	// Mi central
+            Centralita c = new Centralita("Carlitos S.A.");
+
+
+            // Mis 4 llamadas
+            Local l1 = new Local("Bernal", "Rosario", 30, 2.65f);
+            Local l2 = new Local("Lanús", "San Rafael", 45, 1.99f);
+            Local l3 = l2;
+
+            Provincial l4 = new Provincial("Morón", "Bernal", 21, Provincial.Franja.Franja_1);
+            Provincial l5 = new Provincial(l2, Provincial.Franja.Franja_3);
+            Provincial l6 = new Provincial("Costa del Este", "Banfield", 21, Provincial.Franja.Franja_3);
+
+            try
+            {
+                c += l1;
+                c += l1;
+                c += l2;
+                c += l3;
+                c += l4;
+                c += l5;
+                c += l6;
+            }
+            catch (CentralitaException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine(c.ToString());
+            Console.WriteLine("Guardado Exitoso: " + c.Guardar());
+        }
+    }
+}
