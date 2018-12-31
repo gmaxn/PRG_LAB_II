d3e5a8adc85ce60fcbe4ddd7867c4fc6bcd00dd8
Max <maxi.ng@hotmail.com>
2018-12-30 23:33:06 -0300

    -

---
+++ E58/FrmContadorDePalabras/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MiBiblioteca;
+
+
+namespace FrmContadorDePalabras
+{
+    public partial class Form1 : Form
+    {
+        private string rutaArchivoActual;
+        int cursorPosition;
+        public Form1()
+        {
+            InitializeComponent();
+            this.tssl_numberOfChars.Text = "caracteres: 0 - " + this.rutaArchivoActual;
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "txt files (*.txt)|*.txt|dat files (*.dat)|*dat|All files (*.*)|*.*";
+            ofd.ShowDialog();
+            rutaArchivoActual = ofd.FileName;
+
+            rtb_texto.Text = ArchivoTexto.Leer(rutaArchivoActual);
+        }
+
+        private void guardarCtrlSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (rutaArchivoActual == null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "txt files (*.txt)|*.txt|dat files (*.dat)|*dat|All files (*.*)|*.*";
+
+                sfd.ShowDialog();
+                rutaArchivoActual = sfd.FileName;
+                ArchivoTexto.Guardar(rutaArchivoActual, rtb_texto.Text, false);
+            }
+            else
+            {
+                ArchivoTexto.Guardar(rutaArchivoActual, rtb_texto.Text, true);
+            }
+        }
+
+        private void rtb_texto_TextChanged(object sender, EventArgs e)
+        {
+            this.tssl_numberOfChars.Text = string.Format("caracteres: {0} - {1}", rtb_texto.Text.Length, this.rutaArchivoActual);
+        }
+
+        private void guardarComoCtrlMayúsSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "txt files (*.txt)|*.txt|dat files (*.dat)|*dat|All files (*.*)|*.*";
+            sfd.ShowDialog();
+            rutaArchivoActual = sfd.FileName;
+            ArchivoTexto.Guardar(rutaArchivoActual, rtb_texto.Text, false);
+
+        }
+    }
+}
---
+++ E58/IO/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class ArchivoIncorrectoException : Exception
+    {
+        public ArchivoIncorrectoException(string mensaje)
+            : base(mensaje)
+        {
+
+        }
+        public ArchivoIncorrectoException(string mensaje, Exception innerException)
+            : base(mensaje, innerException)
+        {
+
+        }
+    }
+    public interface IArchivos<T>
+    {
+        bool Guardar(string ruta, T obj);
+        T Leer(string ruta);
+    }
+    public abstract class Archivo
+    {
+        protected virtual bool ValidarArchivo(string ruta)
+        {
+            if(!File.Exists(ruta))
+            {
+                throw new FileNotFoundException();
+            }
+            return true;
+        }
+    }
+    public class PuntoDat : Archivo, IArchivos<PuntoDat>
+    {
+        string contenido;
+        public string Contenido
+        {
+            get { return this.contenido; }
+            set { this.contenido = value; }
+        }
+
+        public bool Guardar(string ruta, PuntoDat obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PuntoDat Leer(string ruta)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool ValidarArchivo(string ruta)
+        {
+            try
+            {
+                base.ValidarArchivo(ruta);
+                if (Path.GetExtension(ruta) == ".dat")
+                {
+                    return true;
+                }
+                throw new ArchivoIncorrectoException("El archivo no es un dat.");
+            }
+            catch (Exception ex)
+            {
+                throw new ArchivoIncorrectoException("El archivo no es correcto.", ex.InnerException);
+            }
+        }
+
+    }
+    public class PuntoTxt : Archivo, IArchivos<string>
+    {
+        public bool Guardar(string ruta, string obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Leer(string ruta)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool ValidarArchivo(string ruta)
+        {
+            try
+            {
+                base.ValidarArchivo(ruta);
+                if (Path.GetExtension(ruta) == ".dat")
+                {
+                    return true;
+                }
+                throw new ArchivoIncorrectoException("El archivo no es un dat.");
+            }
+            catch (Exception ex)
+            {
+                throw new ArchivoIncorrectoException("El archivo no es correcto.", ex.InnerException);
+            }
+        }
+    }
+}
