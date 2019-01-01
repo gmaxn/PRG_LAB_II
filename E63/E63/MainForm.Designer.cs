379b79ce84f8f28457d4d78a9d7a4e597c3e099f
Max <maxi.ng@hotmail.com>
2018-12-31 21:48:09 -0300

    finalizado Ok

---
+++ E63/E63/MainForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace E63
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(282, 223);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Location = new System.Drawing.Point(5, 5);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(53, 17);
+            this.lblHora.TabIndex = 1;
+            this.lblHora.Text = "lblHora";
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "MainForm";
+            this.Text = "Timer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ E63/E63/MainForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+
+namespace E63
+{
+    public partial class MainForm : Form
+    {
+        Thread t;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Asigno la hora al inicio
+            this.AsignarHora();
+
+            // Sin Threads
+            //this.EjecutarAsignarHoraPorSegundo();
+
+
+            // Con Timer
+            //this.Timer.Enabled = true;
+
+
+            // Con Threads
+            t = new Thread(new ParameterizedThreadStart(this.EjecutarAsignarHoraPorSegundo));
+            t.Start(1000);
+        }
+
+
+        private void EjecutarAsignarHoraPorSegundo(object sleep)
+        {
+            do
+            {
+                this.AsignarHora();
+                Thread.Sleep((int)sleep);
+            } while (true);
+        }
+        public void AsignarHora()
+        {
+            if (this.lblHora.InvokeRequired)
+            {
+                this.lblHora.BeginInvoke((MethodInvoker)delegate () { this.lblHora.Text = DateTime.Now.ToString(); });
+            }
+            else
+            {
+                this.lblHora.Text = DateTime.Now.ToString();
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (t.IsAlive)
+                t.Abort();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.AsignarHora();
+        }
+    }
+}
